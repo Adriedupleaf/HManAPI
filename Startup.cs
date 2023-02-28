b2ff514b0a390838f7c90ac4c0869401b1b2f6fe
Adriedupleaf <Adriedupleaf1@gmail.com>
2023-02-28 22:50:31 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HManAPI.Startup))]
+namespace HManAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
