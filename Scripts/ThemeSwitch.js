bef5b5fb7c587aa703904e87aba117a3d77c99ca
Adriedupleaf <80708483+Adriedupleaf@users.noreply.github.com>
2023-05-09 19:53:31 +0300

    button fix

--- Scripts/ThemeSwitch.js
+++ Scripts/ThemeSwitch.js
@@ -1,11 +1,11 @@
 ﻿
-addEventListener("input", (event) => {
-    var element = document.getElementById("navbar");
-    //create an if for find the class name of the element and change it to the other class name
+document.getElementById("toggle").addEventListener("click", (event) => {
+        var element = document.getElementById("navbar");
+        //create an if for find the class name of the element and change it to the other class name
 
     if (element.classList.contains("navbar-dark") && element.classList.contains("bg-dark")){
-        element.className = "navbar navbar-expand-lg navbar-light bg-light";
-    } else {
-        element.className = "navbar navbar-expand-lg navbar-dark bg-dark";
-    }
-});
+            element.className = "navbar navbar-expand-lg navbar-light bg-light";
+        } else {
+            element.className = "navbar navbar-expand-lg navbar-dark bg-dark";
+        }
+    });
