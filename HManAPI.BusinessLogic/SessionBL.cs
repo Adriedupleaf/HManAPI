85090f618f8789c6d515a6a14fc3e87799766226
Jekaa919 <eugeniu.slicari@isa.utm.md>
2023-05-17 02:48:56 +0300

    BL Initializing

--- HManAPI.BusinessLogic/SessionBL.cs
+++ HManAPI.BusinessLogic/SessionBL.cs
@@ -5,10 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using HManAPI.BusinessLogic.Core;
 using HManAPI.BusinessLogic.Interfaces;
+using HManAPI.Domain.Entities.User;
 
 namespace HManAPI.BusinessLogic
 {
-    public class SessionBL : UserApi, ISessionBL
-    {
-    }
+     public class SessionBL : UserApi, ISessionBL
+     {
+          public ULoginResponse UserLogin(ULoginData data)
+          {
+               throw new NotImplementedException();
+          }
+
+          public ULoginResponse UserRegister(URegisterData data)
+          {
+               throw new NotImplementedException();
+          }
+     }
 }
